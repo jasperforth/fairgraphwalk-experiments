2f4d69a515719fd03b1742c077be2b3e7fb2bd93
jasperforth <jasper.forth@me.com>
2025-03-10 14:57:41 +0100

    edit jobscript mem

--- job_script_772g.sh
+++ job_script_772g.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1                # One node per task
 #SBATCH --ntasks=1               # One task per node
 #SBATCH --cpus-per-task=80
-#SBATCH --mem=192G
+#SBATCH --mem=750G
 #SBATCH --array=3            # Array job: 1 experiment ['full']
 #SBATCH --time=240:00:00         # Maximum walltime
 
